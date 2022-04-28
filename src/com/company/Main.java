6684ffb20595e997d8afe68d209380d41a96028a
trundle.moses <trundle.moses@gmail.com>
2022-04-28 11:29:26 -0700

    hghg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
 	adventure main = new adventure("Trip To Whistler", "British Columbia", LocalDate.of(2023, Month.FEBRUARY, 07), adventure.typeOfAdventure.SKI);
     sideQuest side = new sideQuest("THE EXTREMELY HARD POWDER BOWL", "The West Slope", sideQuest.difficulty.EXPERT);
-
+    adventureGear important = new adventureGear("SNOWBOARD", "MY RIDE");
         System.out.println(main.getName());
         System.out.println(main.getLocation());
         System.out.println(main.getDateOfAdventure());
@@ -17,5 +17,6 @@ public class Main {
         System.out.println(side.getLocation());
         System.out.println(side.getType());
         System.out.println(side.isRewardAvailable());
+        System.out.println(adventureGear.getName);
     }
 }
---
+++ src/com/company/adventureGear.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class adventureGear {
+
+
+    private String name;
+    private String type;
+
+    public adventureGear(String name, String type) {
+
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
